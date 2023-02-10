cd0c87b2ba89e97334a873cdc82aa83e8e813117
barteksyko <barteksyko@gmail.com>
2023-02-10 20:38:50 +0100

    .

--- src/app.py
+++ src/app.py
@@ -29,145 +29,112 @@ def flask_app():
     switch_ODZ = Button(11)
 
     app = Flask(__name__)
+    while True:
 
-    @app.route("/", methods=("GET", "POST"))
-    @app.route("/index", methods=["POST", "GET"])
-    def index():
-        if request.method == "POST":
-            current = request.form["current"]
-            current2 = request.form["current2"]
-            current3 = request.form["current3"]
+        @app.route("/", methods=("GET", "POST"))
+        @app.route("/index", methods=["POST", "GET"])
+        def index():
+            if request.method == "POST":
+                current = request.form["current"]
+                current2 = request.form["current2"]
+                current3 = request.form["current3"]
+                return render_template("index.html")
+
             return render_template("index.html")
 
-        return render_template("index.html")
+        @app.route("/slider_update", methods=["POST", "GET"])
+        def slider():
+            received_data = request.data
+            received_data = float(received_data) / 50
+            motor_TO1.forward(received_data)
+            motor_TO1.value = received_data
+            lcd.clear()
+            lcd.message("TO1 Hz: %.2f" % (received_data * 50))
 
-    @app.route("/slider_update", methods=["POST", "GET"])
-    def slider():
-        received_data = request.data
-        received_data = float(received_data) / 50
-        motor_TO1.forward(received_data)
-        motor_TO1.value = received_data
-        lcd.clear()
-        lcd.message("TO1 Hz: %.2f" % (received_data * 50))
+            return str(received_data)
 
-        return str(received_data)
+        @app.route("/slider_update2", methods=["POST", "GET"])
+        def slider2():
+            received_data = request.data
+            received_data = float(received_data) / 50
+            motor_TO2.forward(received_data)
+            motor_TO2.value = received_data
+            lcd2.clear()
+            lcd2.message("TO2: %.2f" % (received_data * 50))
+            return str(received_data)
 
-    @app.route("/slider_update2", methods=["POST", "GET"])
-    def slider2():
-        received_data = request.data
-        received_data = float(received_data) / 50
-        motor_TO2.forward(received_data)
-        motor_TO2.value = received_data
+        @app.route("/slider_update3", methods=["POST", "GET"])
+        def slider3():
+            received_data = request.data
+            received_data = float(received_data) / 50
+            motor_ODZ.forward(received_data)
+            motor_ODZ.value = received_data
 
-        return str(received_data)
+            return str(received_data)
 
-    @app.route("/slider_update3", methods=["POST", "GET"])
-    def slider3():
-        received_data = request.data
-        received_data = float(received_data) / 50
-        motor_ODZ.forward(received_data)
-        motor_ODZ.value = received_data
+        @app.route("/check_update", methods=["POST", "GET"])
+        def check():
+            received_data = request.data
+            received_data = str(received_data).replace("'", "")[1:]
 
-        return str(received_data)
+            if received_data == "false":
+                work_led_TO1.off()
+                motor_TO1.stop()
+                work_led_TO2.off()
+                motor_TO2.stop()
+                work_led_ODZ.off()
+                motor_ODZ.stop()
 
-    @app.route("/check_update", methods=["POST", "GET"])
-    def check():
-        received_data = request.data
-        received_data = str(received_data).replace("'", "")[1:]
+            elif received_data == "true":
+                work_led_TO1.on()
+                motor_TO1.forward()
 
-        if received_data == "false":
-            work_led_TO1.off()
-            motor_TO1.stop()
-            work_led_TO2.off()
-            motor_TO2.stop()
-            work_led_ODZ.off()
-            motor_ODZ.stop()
+            return received_data
 
-        elif received_data == "true":
-            work_led_TO1.on()
-            motor_TO1.forward()
+        @app.route("/check_update2", methods=["POST", "GET"])
+        def check2():
+            received_data = request.data
+            received_data = str(received_data).replace("'", "")[1:]
 
-        return received_data
+            if received_data == "false":
+                work_led_TO2.off()
+                motor_TO2.stop()
 
-    @app.route("/check_update2", methods=["POST", "GET"])
-    def check2():
-        received_data = request.data
-        received_data = str(received_data).replace("'", "")[1:]
+            elif received_data == "true":
+                work_led_TO2.on()
+                motor_TO2.forward()
 
-        if received_data == "false":
-            work_led_TO2.off()
-            motor_TO2.stop()
+            return received_data
 
-        elif received_data == "true":
-            work_led_TO2.on()
-            motor_TO2.forward()
+        @app.route("/check_update3", methods=["POST", "GET"])
+        def check3():
+            received_data = request.data
+            received_data = str(received_data).replace("'", "")[1:]
 
-        return received_data
+            if received_data == "false":
+                work_led_ODZ.off()
+                motor_ODZ.stop()
 
-    @app.route("/check_update3", methods=["POST", "GET"])
-    def check3():
-        received_data = request.data
-        received_data = str(received_data).replace("'", "")[1:]
+            elif received_data == "true":
+                work_led_ODZ.on()
+                motor_ODZ.forward()
 
-        if received_data == "false":
-            work_led_ODZ.off()
-            motor_ODZ.stop()
+            return received_data
 
-        elif received_data == "true":
-            work_led_ODZ.on()
-            motor_ODZ.forward()
+        @app.route("/check_update4", methods=["POST", "GET"])
+        def check4():
+            received_data = request.data
+            received_data = str(received_data).replace("'", "")[1:]
 
-        return received_data
+            if received_data == "false":
+                work_led_ODZ.off()
+                motor_ODZ.stop()
 
-    # @app.route("/check_update4", methods=["POST", "GET"])
-    # def check4():
-    #     received_data = request.data
-    #     received_data = str(received_data).replace("'", "")[1:]
+            elif received_data == "true":
+                if switch_ODZ.is_pressed:
+                    work_led_ODZ.on()
+                    motor_ODZ.forward()
 
-    #     # if received_data == "false":
-    #     #     work_led_ODZ.off()
-    #     #     motor_ODZ.stop()
+            return received_data
 
-    #     # elif received_data == "true":
-    #     #     work_led_ODZ.on()
-    #     #     motor_ODZ.forward()
-    #     while True:
-    #         if switch_TO1.is_pressed:
-    #             work_led_TO1.on()
-    #             motor_TO1.forward(potenciometer1_TO1.value)
-    #             print(type(potenciometer1_TO1.value), potenciometer1_TO1.value)
-    #             lcd.clear()
-    #             lcd.message("TO1 Hz: %.2f" % (potenciometer1_TO1.value * 50))
-    #             if swtich_TO2.is_pressed:
-    #                 work_led_TO2.on()
-    #                 motor_TO2.forward(potenciometer2_TO2.value)
-    #                 lcd2.clear()
-    #                 lcd2.message("TO2: %.2f" % (potenciometer2_TO2.value * 50))
-    #                 if switch_ODZ.is_pressed:
-    #                     work_led_ODZ.on()
-    #                     motor_ODZ.forward(potenciometer3_ODZ.value)
-    #                 else:
-    #                     work_led_ODZ.off()
-    #                     motor_ODZ.stop()
-    #             else:
-    #                 work_led_TO2.off()
-    #                 work_led_ODZ.off()
-    #                 motor_TO2.stop()
-    #                 motor_ODZ.stop()
-    #                 lcd2.clear()
-    #                 lcd2.message("TO2 Hz: %.2f" % (0))
-    #         else:
-    #             work_led_TO1.off()
-    #             work_led_TO2.off()
-    #             work_led_ODZ.off()
-    #             motor_TO1.stop()
-    #             motor_TO2.stop()
-    #             motor_ODZ.stop()
-    #             lcd.clear()
-    #             lcd.message("TO1 Hz: %.2f" % (0))
-    #             lcd2.clear()
-    #             lcd2.message("TO2 Hz: %.2f" % (0))
-    #         sleep(0.05)
-    # return received_data
-
-    app.run()
+        app.run()
